0b17b35a9b44366532675ee03675d407152bfdff
marcelo marques <marceloninho@gmail.com>
2021-02-24 23:56:46 -0300

    Adicionar arquivos de projeto.

---
+++ CryptographyHelper/Helpers/CriptographyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryptographyHelper.Helpers {
+    public class CriptographyHelper {
+        private static Random random = new Random();
+        public static string GetRandomKey() {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, 16)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
